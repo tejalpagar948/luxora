bed0a42e6a825b640a99201d2ee2dc5e77743a9d
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:16 +0530

    chore: update backend/models/product-model.js

--- backend/models/product-model.js
+++ backend/models/product-model.js
@@ -3,14 +3,10 @@ const mongoose = require("mongoose");
 const productSchema = mongoose.Schema({
     title: String,
     description: String,
+    category: String,
     price: Number,
     stock: Number,
-    image: String,
-    bg_color: String,
-    discount: {
-        type: Number,
-        default: 0
-    },
+    image: String
 })
 
 module.exports = mongoose.model("product", productSchema);
