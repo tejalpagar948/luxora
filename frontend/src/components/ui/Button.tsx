8b28d6a9af607e520d1c84df18f30a43d6098df5
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:24 +0530

    chore: update frontend/src/components/ui/Button.tsx

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -13,11 +13,11 @@ export const Button: React.FC<ButtonProps> = ({
   ...props
 }) => {
   const baseStyle = 'font-body text-button px-6 py-3 rounded-md transition-all duration-300 focus:outline-none uppercase font-semibold text-center inline-block cursor-pointer';
-  
+
   const variants = {
     primary: 'bg-primary text-background hover:bg-neutral-800 border border-primary',
-    secondary: 'bg-secondary text-primary hover:bg-neutral-200 border border-secondary',
-    accent: 'bg-accent text-background hover:bg-amber-600 border border-accent',
+    secondary: 'bg-secondary text-primary hover:bg-neutral  -200 border border-secondary',
+    accent: 'bg-accent text-background hover:bg-[#E2AD2F] ',
     outline: 'bg-transparent text-primary hover:bg-primary hover:text-background border border-primary',
   };
 
