1f8d0d7746532caeb0f365fe3c34db8f18c4e647
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:15 +0530

    chore: update backend/config/db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,10 +5,10 @@ const mongoose = require('mongoose');
  */
 const connectDB = async () => {
   try {
-    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/luxora';
-    
+    const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/luxoraDB';
+
     const conn = await mongoose.connect(mongoURI);
-    
+
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB Connection Error: ${error.message}`);
