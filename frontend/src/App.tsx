1a62f557061e6b34a48d2caf37708155cdec1d62
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:21 +0530

    chore: update frontend/src/App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { Toaster } from 'react-hot-toast';
+import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <AuthProvider>
+      <CartProvider>
+        <Toaster position="top-right" />
+        <RouterProvider router={router} />
+      </CartProvider>
+    </AuthProvider>
+  );
 }
 
 export default App;
