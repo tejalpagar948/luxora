18330e380f589dc90eff34a32780934a0c5804b7
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:21 +0530

    chore: update frontend/services/productService.js

--- frontend/services/productService.js
+++ frontend/services/productService.js
@@ -3,3 +3,23 @@ import api from "./api";
 export const getProducts = () => {
     return api.get("/products");
 }
+
+export const createProduct = (productData) => {
+    return api.post("/products/create", productData)
+}
+
+export const getProductById = (id) => {
+    return api.get(`/products/edit/${id}`)
+}
+
+export const updateProduct = (id, productData) => {
+    return api.put(`/products/edit/${id}`, productData)
+}
+
+export const deleteProduct = (id) => {
+    return api.delete(`/products/delete/${id}`)
+}
+
+export const getSingleProduct = (id) => {
+    return api.get(`/products/${id}`)
+}
