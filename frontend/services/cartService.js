357edd1f9be59f0b4e0cd43686fdc0aca2ab069b
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:35 +0530

    chore: create frontend/services/cartService.js

---
+++ frontend/services/cartService.js
@@ -0,0 +1,21 @@
+import api from "./api"
+
+export const addToCart = ({ id, quantity }) => {
+    return api.post(`/cart/${id}`, { quantity })
+}
+
+export const getCart = () => {
+    return api.get(`/cart`)
+}
+
+export const deleteFromCart = (id) => {
+    return api.delete(`/cart/${id}`)
+}
+
+export const updateCart = (id, quantity) => {
+    return api.put(`/cart/${id}`, { quantity })
+}
+
+export const deleteManyFromCart = (ids) => {
+    return api.post(`/cart/delete-many`, { ids })
+}
