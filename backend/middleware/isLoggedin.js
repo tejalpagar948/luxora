0234151ad7c7331700d9542458371c12bc7fb0fc
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-11 23:47:16 +0530

    chore: update backend/middleware/isLoggedin.js

--- backend/middleware/isLoggedin.js
+++ backend/middleware/isLoggedin.js
@@ -5,6 +5,7 @@ module.exports = async (req, res, next) => {
     const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({
+            success: false,
             message: "Unauthorized"
         });
     }
