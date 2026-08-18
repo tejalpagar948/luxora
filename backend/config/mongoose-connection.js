37c9a8590c7996e53385ce4c07f4571545f28d8a
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-19 01:29:10 +0530

    fix deployment issue

--- backend/config/mongoose-connection.js
+++ backend/config/mongoose-connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dbgr = require("debug")("development:mongoose");
 const config = require("config");
 
-mongoose.connect(`${config.get("MONGODB_URI")}/luxoraDB`).then(() => {
+mongoose.connect(process.env.MONGODB_URI).then(() => {
     dbgr("MongoDB connected successfully");
 }).catch(() => {
     dbgr("MongoDB connection failed");
