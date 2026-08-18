78b53cd03d2b06c97e05d5c44d5210708394b6ec
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-19 00:29:18 +0530

    give dynamic url

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,8 +1,7 @@
-// api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true,
 });
 
