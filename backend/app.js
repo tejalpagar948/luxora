8bd1287195a810112b9309159c87c206eb644890
tejalpagar948 <tejalpagar948@gmail.com>
2026-08-19 22:25:23 +0530

    fix production issue

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ const express = require('express');
 require("dotenv").config()
 const cookieParser = require('cookie-parser');
 const path = require('path');
-const db = require("./config/mongoose-connection");
 const ownersRouter = require('./routes/ownersRouter');
 const productsRouter = require('./routes/productsRouter');
 const usersRouter = require('./routes/usersRouter');
@@ -85,6 +84,4 @@ app.use("/wishlist", wishlistRouter)
 //   });
 // });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+module.exports = app;
